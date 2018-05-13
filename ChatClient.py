6d4c2cd741ad467b9ecc9a56d4ab3ca36e36cbc9
Unknown <misha@melnyk.org>
2018-05-13 14:16:58 -0400

    True Initial Commit

---
+++ ChatClient.py
@@ -0,0 +1,123 @@
+import socket
+from tkinter import *
+import threading
+from queue import Queue
+from time import sleep
+
+users = ["Users:"]
+dUsers = []
+
+queueMsg = []
+displayMsg = []
+
+#checkPort = Queue()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+connected = False
+
+def getName(Event):
+    askName.destroy()
+    #print("Got name")
+    global screenName
+    screenName = screenName.get()
+
+def askForName():
+    global askName, screenName
+    askName = Tk()
+    screenName = StringVar()
+    cvAN = Canvas(askName, width = 200, height = 200)
+    cvAN.pack()
+    cvAN.create_text(100, 20, text = "Enter your name:")
+    entryAN = Entry(cvAN, textvariable = screenName, width = 12)
+    entryAN.place(x = 50, y = 100)
+    askName.bind("<Return>", getName)
+    askName.mainloop()
+
+while not connected:
+    try:
+        s.connect(("127.0.0.1",4545))
+        connected = True
+    except socket.error:
+        pass
+
+askForName()
+nameSend = "|User|{}".format(screenName)
+s.send(nameSend.encode())
+
+w = 800
+h = 800
+
+root = Tk()
+cv = Canvas(root, width = w, height = h, bg = "lightblue")
+cv.pack()
+cv.create_rectangle(100,150, w - 100, h, fill = 'white')
+cv.update()
+
+maxMessages = 10
+
+def displayTexts():
+    while True:
+        for i in dUsers:
+            cv.delete(i)
+        for i in users:
+            dUsers.append(cv.create_text(w-50, 150+users.index(i) * 30, text = i))
+        
+        cv.update()
+        
+        for i in displayMsg:
+            cv.delete(i)
+        
+        if len(queueMsg) >= maxMessages:
+            msgToDisplay = queueMsg[-10:]
+        else:
+            msgToDisplay = queueMsg.copy()
+        msgToDisplay.reverse()
+        for i in range(len(msgToDisplay)):
+            displayMsg.append(cv.create_text(w/2, h-40 - i * 20, text = msgToDisplay[i]))
+        
+        cv.update()
+        sleep(0.03)
+
+def recvLoop():
+    while True:
+        data = s.recv(4096)
+        if not data:
+            break
+        msg = data.decode('utf-8')
+        if msg.startswith("|Users|"):
+            global users
+            userList = msg.split("|Users|")
+            print(userList[1])
+            users = eval(userList[1])
+            print(users)
+            print("Got new user list")
+            queueMsg.append("{} has connected".format(users[-1]))
+        else:
+            queueMsg.append(msg)
+
+def sendMsg(Event):
+    global sendmsg
+    msg = sendmsg.get()
+    msgSent = "{}:{}".format(screenName, msg)
+    s.send(msgSent.encode())
+    sendEntry.delete(0, "end")
+
+def sendLoop():
+    global sendmsg, sendEntry
+    sendmsg = StringVar()
+    sendEntry = Entry(cv, textvariable = sendmsg, width = 50)
+    sendEntry.place(x=100, y=h-20)
+    cv.update()
+    root.bind("<Return>", sendMsg)
+
+userThread = threading.Thread(target = displayTexts)
+userThread.daemon = True
+userThread.start()
+
+recvThread = threading.Thread(target=recvLoop)
+recvThread.daemon = True
+recvThread.start()
+
+sendLoop()
+root.mainloop()
---
+++ ChatServer.py
@@ -0,0 +1,70 @@
+import socket
+import threading
+import logging
+import atexit
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def closeSocket():
+    print("Starting to close connections")
+    global s, connections
+    for c in connections:
+        try:
+            c.close()
+        except Exception:
+            pass
+    s.close()
+    print("Finished closing connections")
+
+atexit.register(closeSocket)
+
+host = ""
+port = 4545
+
+connections = []
+names = ["Users:"]
+
+print("Socket Created")
+
+s.bind((host, port))
+
+print("Socket Bound")
+
+s.listen(5)
+
+print("Socket is Listening")
+
+
+def clientHandler(conn,addr):
+    print("Connected with client.\nIP:{}, Port:{}".format(addr[0], addr[1]))
+    while True:
+        data = conn.recv(2048)
+        if not data:
+            break
+        try:
+            data = data.decode('utf-8')#.split("\n")
+        except:
+            break
+        print(data)
+        if data.startswith("|User|"):
+            global names
+            username = data.split("|User|")[1]
+            names.append(username)
+            data = "|Users|{}".format(names)
+        #data.pop(-1)
+        #data = data[0]
+
+        #data = "Server Message: {}\n".format(data)
+        
+        conn.sendall(data.encode())
+    s.sendall("{} has disconnected".format(username).encode())
+    conn.close()
+    print("Disconnected from client.\nIP:{}, Port:{}".format(addr[0], addr[1]))
+    names.remove(username)
+
+
+while True:
+    conn, addr = s.accept()
+    connections.append(conn)
+    thread = threading.Thread(target = clientHandler, args=(conn, addr))
+    thread.start()
